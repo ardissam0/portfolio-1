ccfe5fd3d7b87b3fd203db7015cbd38426b754be
Sam Ardis <ardis.productions@gmail.com>
2020-09-12 19:09:04 -0400

    updated projects section with some simple, projects, this is what I will work on until all my projects and new projects will be on here

--- src/components/projects.js
+++ src/components/projects.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import { Tabs, Tab, Grid, Cell, Card, CardTitle, CardText, CardActions, Button, CardMenu, IconButton } from 'react-mdl';
 import NasaImg from '../img/NasaSnip.png';
+import GithubApiProjImg from '../img/githubapiproj.JPG'
+import MemAddProjImg from '../img/memberaddproj.JPG'
 
 class Projects extends Component {
   constructor(props) {
@@ -34,44 +36,23 @@ class Projects extends Component {
             </CardMenu>
           </Card>
 
-          {/* Project 2 */}
-          <Card shadow={5} style={{minWidth: '450', margin: 'auto'}}>
-            <CardTitle>
-                    <img
-                    src={NasaImg}
-                    alt="Nasa Project"
-                    style={{height: '100%', width: '100%'}}
-                    /></CardTitle>
-            <CardText>
-              Built a NASA photo of the day app that calls NASA's API for a new image, title, and explanation each day.
-            </CardText>
-            <CardText> STACK: React, JavaScript, and CSS.</CardText>
-            <CardActions border>
-              <Button colored><a href="https://nasa-photo-of-the-day-fq1quvy7u.vercel.app/" rel="noopener noreferrer" target="_blank">Deployed</a></Button>
-              <Button colored><a href="https://github.com/ardissam0/nasa-photo-of-the-day" rel="noopener noreferrer" target="_blank">Github</a></Button>
-            </CardActions>
-            <CardMenu style={{color: '#fff'}}>
-              <IconButton name="share" />
-            </CardMenu>
-          </Card>
-
           {/* Project 3 */}
           <Card shadow={5} style={{minWidth: '450', margin: 'auto'}}>
             <CardTitle>
                     <img
-                    src={NasaImg}
-                    alt="Nasa Project"
+                    src={MemAddProjImg}
+                    alt="Member add proj"
                     style={{height: '100%', width: '100%'}}
                     /></CardTitle>
             <CardText>
-              Built a NASA photo of the day app that calls NASA's API for a new image, title, and explanation each day.
+              Built a simple app that you can use a form to add team members to the DOM.
             </CardText>
             <CardText> STACK: React, JavaScript, and CSS.</CardText>
             <CardActions border>
-              <Button colored><a href="https://nasa-photo-of-the-day-fq1quvy7u.vercel.app/" rel="noopener noreferrer" target="_blank">Deployed</a></Button>
-              <Button colored><a href="https://github.com/ardissam0/nasa-photo-of-the-day" rel="noopener noreferrer" target="_blank">Github</a></Button>
+              <Button colored><a href="https://team-builder-app-swart.vercel.app/" rel="noopener noreferrer" target="_blank">Deployed</a></Button>
+              <Button colored><a href="https://github.com/ardissam0/team-builder/tree/Samuel-Ardis" rel="noopener noreferrer" target="_blank">Github</a></Button>
             </CardActions>
-            <CardMenu style={{color: '#fff'}}>
+            <CardMenu style={{color: '#00000'}}>
               <IconButton name="share" />
             </CardMenu>
           </Card>
@@ -81,7 +62,27 @@ class Projects extends Component {
       )
     } else if(this.state.activeTab === 1) {
       return (
-        <div> </div>
+        <div>          
+          {/* Project 1 */}
+        <Card shadow={5} style={{minWidth: '450', margin: 'auto'}}>
+          <CardTitle>
+                  <img
+                  src={GithubApiProjImg}
+                  alt="Github Api Proj"
+                  style={{height: '100%', width: '100%'}}
+                  /></CardTitle>
+          <CardText>
+            Built an app that pulls data form my github and displays me and my followers in cards.
+          </CardText>
+          <CardText> STACK: JavaScript, HTML, and CSS.</CardText>
+          <CardActions border>
+            <Button colored><a href="https://github-usercard-lbjbk61lu.vercel.app/" rel="noopener noreferrer" target="_blank">Deployed</a></Button>
+            <Button colored><a href="https://github.com/ardissam0/github-usercard/tree/Samuel-Ardis" rel="noopener noreferrer" target="_blank">Github</a></Button>
+          </CardActions>
+          <CardMenu style={{color: '#fff'}}>
+            <IconButton name="share" />
+          </CardMenu>
+        </Card> </div>
       )
     } else if(this.state.activeTab === 2) {
       return (
