fa7781bba04d6e811f3bc56ba91c66f34a397fe9
Sam Ardis <ardis.productions@gmail.com>
2020-09-13 22:28:37 -0400

    updated projects some more, adjusted stlying of projects page, working on tabs on project page

--- src/components/projects.js
+++ src/components/projects.js
@@ -1,8 +1,9 @@
 import React, { Component } from 'react';
 import { Tabs, Tab, Grid, Cell, Card, CardTitle, CardText, CardActions, Button, CardMenu, IconButton } from 'react-mdl';
 import NasaImg from '../img/NasaSnip.png';
-import GithubApiProjImg from '../img/githubapiproj.JPG'
-import MemAddProjImg from '../img/memberaddproj.JPG'
+import GithubApiProjImg from '../img/githubapiproj.JPG';
+import MemAddProjImg from '../img/memberaddproj.JPG';
+import MiracleMessagesImg from '../img/miraclemessagessnip.JPG';
 
 class Projects extends Component {
   constructor(props) {
@@ -16,45 +17,57 @@ class Projects extends Component {
       return(
         <div className="projects-grid">
           {/* Project 1 */}
-          <Card shadow={5} style={{minWidth: '450', margin: 'auto'}}>
+          <Card shadow={5} style={{width: '450', margin: 'auto', background: 'black', borderRadius: '7px'}}>
+            <CardTitle>
+                    <img
+                    src={MiracleMessagesImg}
+                    alt="Mirracle messages labs project"
+                    style={{height: '100%', width: '100%'}}
+                    /></CardTitle>
+            <CardText style = {{ color: 'white'}}>
+              Worked with a team of developers to build an app for a nonprofit organization called Miracle Messages. We worked directly with the CEO to build out a map with pins, lines, and pop up modals as well as tiered authorization structure for dashboards and functionality. This was all to show connections between homeless people who have been reconnected with friends and family and to help organize the process for the CEO and others working at the organization.
+            </CardText>
+            <CardText style = {{ color: 'white'}}> STACK: React, JavaScript, Sass.</CardText>
+            <CardActions border className='btn-proj-div'>
+              <button className ='btn-proj'><a href="https://miracle-messages-fe-dgknhsijz.vercel.app/" rel="noopener noreferrer" target="_blank">Deployed</a></button>
+              <button className ='btn-proj'><a href="https://github.com/Lambda-School-Labs/miracle-messages-fe" rel="noopener noreferrer" target="_blank">Github</a></button>
+            </CardActions>
+          </Card>
+
+          {/* Project 2 */}
+          <Card shadow={5} style={{width: '450', margin: 'auto', background: 'black', borderRadius: '7px'}}>
             <CardTitle>
                     <img
                     src={NasaImg}
                     alt="Nasa Project"
                     style={{height: '100%', width: '100%'}}
                     /></CardTitle>
-            <CardText>
-              Built a NASA photo of the day app that calls NASA's API for a new image, title, and explanation each day.
+            <CardText style = {{ color: 'white'}}>
+              I built a photo of the day app that uses NASA's API for an image, title, and explanation that updates daily. I also added a simple animation and made it mobile friendly.
             </CardText>
-            <CardText> STACK: React, JavaScript, and CSS.</CardText>
-            <CardActions border>
-              <Button colored><a href="https://nasa-photo-of-the-day-fq1quvy7u.vercel.app/" rel="noopener noreferrer" target="_blank">Deployed</a></Button>
-              <Button colored><a href="https://github.com/ardissam0/nasa-photo-of-the-day" rel="noopener noreferrer" target="_blank">Github</a></Button>
+            <CardText style = {{ color: 'white'}}> STACK: React, JavaScript, and CSS.</CardText>
+            <CardActions border className='btn-proj-div'>
+              <button className ='btn-proj'><a href="https://nasa-photo-of-the-day-fq1quvy7u.vercel.app/" rel="noopener noreferrer" target="_blank">Deployed</a></button>
+              <button className ='btn-proj'><a href="https://github.com/ardissam0/nasa-photo-of-the-day" rel="noopener noreferrer" target="_blank">Github</a></button>
             </CardActions>
-            <CardMenu style={{color: '#fff'}}>
-              <IconButton name="share" />
-            </CardMenu>
           </Card>
 
           {/* Project 3 */}
-          <Card shadow={5} style={{minWidth: '450', margin: 'auto'}}>
+          <Card shadow={5} style={{width: '450', margin: 'auto', background: 'black', borderRadius: '7px'}}>
             <CardTitle>
                     <img
                     src={MemAddProjImg}
                     alt="Member add proj"
                     style={{height: '100%', width: '100%'}}
                     /></CardTitle>
-            <CardText>
-              Built a simple app that you can use a form to add team members to the DOM.
+            <CardText style = {{ color: 'white'}}>
+              I built a simple app where you can use a form to add team members to the DOM.
             </CardText>
-            <CardText> STACK: React, JavaScript, and CSS.</CardText>
-            <CardActions border>
-              <Button colored><a href="https://team-builder-app-swart.vercel.app/" rel="noopener noreferrer" target="_blank">Deployed</a></Button>
-              <Button colored><a href="https://github.com/ardissam0/team-builder/tree/Samuel-Ardis" rel="noopener noreferrer" target="_blank">Github</a></Button>
+            <CardText style = {{ color: 'white'}}> STACK: React, JavaScript, and CSS.</CardText>
+            <CardActions border className='btn-proj-div'>
+              <button className ='btn-proj'><a href="https://team-builder-app-swart.vercel.app/" rel="noopener noreferrer" target="_blank">Deployed</a></button>
+              <button className ='btn-proj'><a href="https://github.com/ardissam0/team-builder/tree/Samuel-Ardis" rel="noopener noreferrer" target="_blank">Github</a></button>
             </CardActions>
-            <CardMenu style={{color: '#00000'}}>
-              <IconButton name="share" />
-            </CardMenu>
           </Card>
         </div>
 
@@ -63,25 +76,22 @@ class Projects extends Component {
     } else if(this.state.activeTab === 1) {
       return (
         <div>          
-          {/* Project 1 */}
-        <Card shadow={5} style={{minWidth: '450', margin: 'auto'}}>
+          {/* Project 4 */}
+        <Card shadow={5} style={{width: '450', margin: 'auto', background: 'black', borderRadius: '7px'}}>
           <CardTitle>
                   <img
                   src={GithubApiProjImg}
                   alt="Github Api Proj"
                   style={{height: '100%', width: '100%'}}
                   /></CardTitle>
-          <CardText>
-            Built an app that pulls data form my github and displays me and my followers in cards.
+          <CardText style = {{ color: 'white'}}>
+            I built an app that pulls data form my github and displays me and my followers in cards.
           </CardText>
-          <CardText> STACK: JavaScript, HTML, and CSS.</CardText>
-          <CardActions border>
-            <Button colored><a href="https://github-usercard-lbjbk61lu.vercel.app/" rel="noopener noreferrer" target="_blank">Deployed</a></Button>
-            <Button colored><a href="https://github.com/ardissam0/github-usercard/tree/Samuel-Ardis" rel="noopener noreferrer" target="_blank">Github</a></Button>
+          <CardText style = {{ color: 'white'}}> STACK: JavaScript, HTML, and CSS.</CardText>
+          <CardActions border className='btn-proj-div'>
+            <button className ='btn-proj'><a href="https://github-usercard-lbjbk61lu.vercel.app/" rel="noopener noreferrer" target="_blank">Deployed</a></button>
+            <button className ='btn-proj'><a href="https://github.com/ardissam0/github-usercard/tree/Samuel-Ardis" rel="noopener noreferrer" target="_blank">Github</a></button>
           </CardActions>
-          <CardMenu style={{color: '#fff'}}>
-            <IconButton name="share" />
-          </CardMenu>
         </Card> </div>
       )
     } else if(this.state.activeTab === 2) {
@@ -102,10 +112,10 @@ class Projects extends Component {
     return(
       <div>
         <Tabs activeTab={this.state.activeTab} onChange={(tabId) => this.setState({ activeTab: tabId })} ripple>
-          <Tab>React</Tab>
-          <Tab>JavaScript</Tab>
-          <Tab>Node</Tab>
-          <Tab>Python</Tab>
+          <Tab style={{textDecoration: 'none', color: 'black', fontWeight: 'bold'}}>React</Tab>
+          <Tab style={{textDecoration: 'none', color: 'black', fontWeight: 'bold'}}>JavaScript</Tab>
+          <Tab style={{textDecoration: 'none', color: 'black', fontWeight: 'bold'}}>Node</Tab>
+          <Tab style={{textDecoration: 'none', color: 'black', fontWeight: 'bold'}}>Python</Tab>
         </Tabs>
 
 
