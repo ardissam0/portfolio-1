f1db0062b87056b9d24c0a9d3dd93b8dc0e42cda
Sam Ardis <ardis.productions@gmail.com>
2020-09-15 00:41:27 -0400

    Added another project

--- src/components/projects.js
+++ src/components/projects.js
@@ -4,6 +4,8 @@ import NasaImg from '../img/NasaSnip.png';
 import GithubApiProjImg from '../img/githubapiproj.JPG';
 import MemAddProjImg from '../img/memberaddproj.JPG';
 import MiracleMessagesImg from '../img/miraclemessagessnip.JPG';
+import TodoAppImg from '../img/todoappimg.JPG'
+import CarSalesImg from '../img/carsalesimg.JPG'
 
 class Projects extends Component {
   constructor(props) {
@@ -53,6 +55,42 @@ class Projects extends Component {
           </Card>
 
           {/* Project 3 */}
+          <Card shadow={5} className="proj-cards" style={{width: '450', margin: 'auto', background: 'black', borderRadius: '7px', marginBottom: '25px'}}>
+            <CardTitle>
+                    <img
+                    src={TodoAppImg}
+                    alt="Todo app proj"
+                    style={{height: '100%', width: '100%'}}
+                    /></CardTitle>
+            <CardText style = {{ color: 'white'}}>
+              I built a simple todo app, you can add and delete your daily todo's.
+            </CardText>
+            <CardText style = {{ color: 'white'}}> STACK: React, Redux, JavaScript, and CSS.</CardText>
+            <CardActions border className='btn-proj-div'>
+              <button className ='btn-proj'><a href="https://todo-app-1.vercel.app/" rel="noopener noreferrer" target="_blank">Deployed</a></button>
+              <button className ='btn-proj'><a href="https://github.com/ardissam0/todo-app-1" rel="noopener noreferrer" target="_blank">Github</a></button>
+            </CardActions>
+          </Card>
+
+          {/* Project 4 */}
+          <Card shadow={5} className="proj-cards" style={{width: '450', margin: 'auto', background: 'black', borderRadius: '7px', marginBottom: '25px'}}>
+            <CardTitle>
+                    <img
+                    src={CarSalesImg}
+                    alt="Car sales proj"
+                    style={{height: '100%', width: '100%'}}
+                    /></CardTitle>
+            <CardText style = {{ color: 'white'}}>
+              I built an app where you can add and delete different features on a Ford Mustang and the total price will be added up for you.
+            </CardText>
+            <CardText style = {{ color: 'white'}}> STACK: React, Redux, JavaScript, and Sass.</CardText>
+            <CardActions border className='btn-proj-div'>
+              <button className ='btn-proj'><a href="https://car-sales-ten.now.sh/" rel="noopener noreferrer" target="_blank">Deployed</a></button>
+              <button className ='btn-proj'><a href="https://github.com/ardissam0/Car-Sales" rel="noopener noreferrer" target="_blank">Github</a></button>
+            </CardActions>
+          </Card>
+
+          {/* Project 5 */}
           <Card shadow={5} className="proj-cards" style={{width: '450', margin: 'auto', background: 'black', borderRadius: '7px', marginBottom: '25px'}}>
             <CardTitle>
                     <img
