5f416b59469073bbeef75fd0ea9079a6fcc9ca01
Sam Ardis <ardis.productions@gmail.com>
2020-09-22 20:55:10 -0400

    Something is wrong, trying to fix file issues

--- src/components/main.js
+++ src/components/main.js
@@ -2,19 +2,19 @@ import React from 'react';
 import { Switch, Route } from 'react-router-dom';
 
 import LandingPage from './landingpage';
-import AboutMe from './AboutMe';
-import Contact from './Contact';
-import Projects from './Projects';
-import Resume from './Resume';
+import AboutMe from './aboutme';
+import Contact from './contact';
+import Projects from './projects';
+import Resume from './resume';
 
 
 const Main = () => (
   <Switch>
     <Route exact path="/" component={LandingPage} />
-    <Route path="/AboutMe" component={AboutMe} />
-    <Route path="/Contact" component={Contact} />
-    <Route path="/Projects" component={Projects} />
-    <Route path="/Resume" component={Resume} />
+    <Route path="/aboutme" component={AboutMe} />
+    <Route path="/contact" component={Contact} />
+    <Route path="/projects" component={Projects} />
+    <Route path="/resume" component={Resume} />
   </Switch>
 )
 
