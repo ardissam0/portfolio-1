18dc09cf58949096c3311b775e3f9bc2c70373fb
Sam Ardis <ardis.productions@gmail.com>
2020-09-22 20:43:28 -0400

    Something is wrong with the file not being able to be found, trying to fix it, everything is running great locally

--- src/components/main.js
+++ src/components/main.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Switch, Route } from 'react-router-dom';
 
-import LandingPage from './Landingpage';
+import LandingPage from './LandingPage';
 import AboutMe from './AboutMe';
 import Contact from './Contact';
 import Projects from './Projects';
