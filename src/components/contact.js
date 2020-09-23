cfe297d51939a855c4438ec44ececf7bbe14b0e8
Sam Ardis <ardis.productions@gmail.com>
2020-09-22 22:55:56 -0400

    Added changes to footer and contact page since components are having issues rendering with footer

--- src/components/contact.js
+++ src/components/contact.js
@@ -6,42 +6,41 @@ class Contact extends Component {
   render() {
     return(
       <div className="contact-body">
-        <Layout>
-            <Content>
-            <Grid className="contact-grid">
-              <Cell col={6}>
-              <h2>Samuel Ardis</h2>
-                <p style={{ width: '95%', margin: 'auto', }}> Have a question or want to work together?</p>
-              <div className="contact-list">
-              <h2>Contact Me</h2>
-                <hr/>
-                <br/>
-              <ContactForm/>
-                  <List style={{ color: 'black',}}>
-                    <ListItem>
-                      <ListItemContent style={{fontSize: '25px', fontFamily: 'Anton', color: 'black',}}>
-                        <i className="fa fa-phone-square" aria-hidden="true"/>
-                        (517) 240-2202
-                      </ListItemContent>
-                    </ListItem>
+          <Grid className="contact-grid">
+            <Layout>
+              <Content>
+                <Cell col={6}>
+                <h2>Samuel Ardis</h2>
+                  <p style={{ width: '95%', margin: 'auto', }}> Have a question or want to work together?</p>
+                <div className="contact-list">
+                <h2>Contact Me</h2>
+                  <hr/>
+                <ContactForm/>
+                    <List style={{ color: 'black',}}>
+                      <ListItem>
+                        <ListItemContent style={{fontSize: '25px', fontFamily: 'Anton', color: 'black',}}>
+                          <i className="fa fa-phone-square" aria-hidden="true"/>
+                          (517) 240-2202
+                        </ListItemContent>
+                      </ListItem>
 
-                    <ListItem>
-                      <ListItemContent style={{fontSize: '25px', fontFamily: 'Anton', color: 'black',}}>
-                        <i className="fa fa-envelope" aria-hidden="true"/>
-                        ardis.productions@gmail.com
-                      </ListItemContent>
-                    </ListItem>
+                      <ListItem>
+                        <ListItemContent style={{fontSize: '25px', fontFamily: 'Anton', color: 'black',}}>
+                          <i className="fa fa-envelope" aria-hidden="true"/>
+                          ardis.productions@gmail.com
+                        </ListItemContent>
+                      </ListItem>
 
-                    <ListItem>
-                      <ListItemContent style={{fontSize: '25px', fontFamily: 'Anton', color: 'black',}}>
-                      </ListItemContent>
-                    </ListItem>
-                  </List>
-                </div>
-              </Cell>
-            </Grid>
-          </Content>
-        </Layout>
+                      <ListItem>
+                        <ListItemContent style={{fontSize: '25px', fontFamily: 'Anton', color: 'black',}}>
+                        </ListItemContent>
+                      </ListItem>
+                    </List>
+                  </div>
+                </Cell>
+              </Content>
+            </Layout>
+          </Grid>
       </div>
     )
   }
